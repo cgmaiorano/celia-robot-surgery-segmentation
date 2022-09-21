d1a6cd0f18e0466dfd65d431bb36945d16a22ad0
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2022-09-21 16:34:33 -0500

    Update train.bash

--- train.bash
+++ train.bash
@@ -1,5 +1,5 @@
 for i in 0 1
 do
     echo "Iteration $i"
-    python3 train.py --device-ids 0 --batch-size 3 --workers 12 --lr 0.00001 --fold $i --n-epochs 20 --jaccard-weight 1
+    python3 train.py --device-ids 0 --batch-size 5 --workers 12 --lr 0.00001 --fold $i --n-epochs 20 --jaccard-weight 1
 done
